e47d4bddd53f0d298b08492722edbb4ad6cd9b04
Nathan-Houel <houel.nathan.18@gmail.com>
2025-12-02 01:47:20 +0100

    Modify dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 from generator import systems
 
+
 def load_data(sys_name):
     conn = sqlite3.connect('avionics.db')
     data = pd.read_sql(f"SELECT Equipments.Name_equipment, Subsystems.Name, Limits.measure_type, Limits.min_value, Limits.max_value \
@@ -11,7 +12,20 @@ def load_data(sys_name):
                        WHERE Subsystems.Name = ?", conn, params=(sys_name,))
     return data
 
+
 st.title("Equipments's table")
 selected_system = st.sidebar.selectbox("Choose a system", systems)
+show_type = st.sidebar.checkbox("Measure type")
+show_min = st.sidebar.checkbox("Min value")
+show_max = st.sidebar.checkbox("Max value")
+
 df = load_data(selected_system)
+
+if show_type == False :
+    df = df.drop(columns=['measure_type'])
+if show_min == False :
+    df = df.drop(columns=['min_value'])
+if show_max == False :
+    df = df.drop(columns=['max_value'])
+
 st.dataframe(df)
